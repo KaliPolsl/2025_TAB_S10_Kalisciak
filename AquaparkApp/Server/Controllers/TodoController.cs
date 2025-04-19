51b90a11ba1d6d169e9569eb0b6fe934232d8a29
Cegla <53154507+kamilceglarski@users.noreply.github.com>
2025-04-19 12:04:55 +0200

    new configuration

---
+++ AquaparkApp/Server/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿// Server/Controllers/TodoController.cs
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TodoController : ControllerBase
+{
+    private static List<TodoItem> items = new()
+    {
+        new TodoItem { Id = 1, Title = "Test", IsDone = false }
+    };
+
+    [HttpGet]
+    public IEnumerable<TodoItem> Get() => items;
+
+    [HttpPost]
+    public IActionResult Add(TodoItem item)
+    {
+        item.Id = items.Max(i => i.Id) + 1;
+        items.Add(item);
+        return Ok(item);
+    }
+}
---
+++ AquaparkApp/Shared/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿/* Shared classes can be referenced by both the Client and Server */
+// Shared/TodoItem.cs
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool IsDone { get; set; }
+}
