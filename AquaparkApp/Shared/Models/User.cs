398f78176af953592c836dd3a64751816e31e8b2
antosiowsky <115071865+antosiowsky@users.noreply.github.com>
2025-04-26 20:00:46 +0200

    auth-system done, database need to be configured, connection to bd almost done

---
+++ AquaparkApp/Shared/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaparkApp.Shared.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        public string PasswordHash { get; set; } = ""; // Hasło powinno być przechowywane jako hash
+    }
+
+}
