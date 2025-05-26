63b0cda68634ba59dd92c515900b1c5adcca1d61
Cegla <53154507+kamilceglarski@users.noreply.github.com>
2025-05-26 13:47:09 +0200

    attractions page added

---
+++ AquaparkApp/Server/Controllers/AtrakcjaController.cs
@@ -0,0 +1,25 @@
+﻿using AquaparkApp.Server.Data;
+using AquaparkApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AquaparkApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtrakcjaController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AtrakcjaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Atrakcja>>> GetAtrakcje()
+        {
+            return await _context.Atrakcja.ToListAsync();
+        }
+    }
+}
---
+++ AquaparkApp/Shared/Atrakcja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquaparkApp.Shared
+{
+    public class Atrakcja
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string? Opis { get; set; }
+        public int MaxOsób { get; set; }
+        public bool WymagaDodatkowejOplaty { get; set; }
+        public decimal? CenaDodatkowa { get; set; }
+    }
+}
